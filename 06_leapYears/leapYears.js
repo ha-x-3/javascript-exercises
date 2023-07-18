a49b50ff2d57f538e15175d3cad478498e185996
Brittney Epperson <109298052+ha-x-3@users.noreply.github.com>
2023-07-18 16:00:45 -0500

    Synced fork for updates

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  } else{
+    return false;
+  }
 };
+
+/* Odin Solution VVVV*********
+
+const leapYears = function(year) {
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+};
+
+leapYears(100)
+*/
 
 // Do not edit below this line
 module.exports = leapYears;
