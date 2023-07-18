b6d93e843db6f68a59b834d1ea5040d3af68fb1a
Brittney Epperson <109298052+ha-x-3@users.noreply.github.com>
2023-07-18 15:54:46 -0500

    Synced fork for updates

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,27 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else if (num === 0) {
+    return "";
+  } else if (num === 1) {
+    return string;
+  } else {
+    return string + repeatString(string, num - 1);
+  }
 };
+
+/* Odin Solution VVVVVVVVVVVVVV**********************
+
+const repeatString = function(word, times) {
+  if (times < 0) return 'ERROR';
+  let string = '';
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
+};
+
+*/
 
 // Do not edit below this line
 module.exports = repeatString;
