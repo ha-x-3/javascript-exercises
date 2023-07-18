8ffb9bd92defaa203fccbc240856690a678ef64b
Brittney Epperson <109298052+ha-x-3@users.noreply.github.com>
2023-07-18 15:56:14 -0500

    Synced fork for updates

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  return string.split('').reverse().join('');
 };
+
+console.log(reverseString("hello"));
+
+/* Alternate Solution VVVVVV******
+
+const reverseString = function(text) {
+  const myArray = text.split("");
+  let reverseMyArray = myArray.reverse().join("");
+  return reverseMyArray;
+};
+
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
