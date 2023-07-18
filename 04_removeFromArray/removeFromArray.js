4abb57d8f3be4e8e57c7881e896e6e204f66cb1b
Brittney Epperson <109298052+ha-x-3@users.noreply.github.com>
2023-07-18 15:57:54 -0500

    Synced fork for updates

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,40 @@
-const removeFromArray = function() {
-
+// Using Rest Operator
+const removeFromArray = function(...args) {
+  //the first item in our list of args is the array - pull it out
+  const array = args[0];
+  //create new empty array
+  const newArray = [];
+  //use forEach to go through the array
+  array.forEach((item) => {
+    //push every element in new array UNLESS it is included in the function arguements
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  //return new array
+  return newArray;
 };
+
+/* Additional Solutions VVVV*****************
+
+const removeFromArray = function(arr) {
+  //Construct an array of arguments
+  let badValues = Array.prototype.slice.call(arguments);
+  //Get rid of the first "good" array element
+  badValues.shift();
+  //Make "bad" values false and thus filter them
+  return arr.filter(function(x) {
+    return badValues.indexOf(x) == -1;
+  });
+}
+
+More advanced Method:
+var removeFromArray = function(...args) {
+  const array = args[0];
+  return array.filter(val => !args.includes(val))
+}
+
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
