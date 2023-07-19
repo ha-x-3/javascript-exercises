b3eaa3a6ef07cdb11a2ad2b25f6a661fa1431cfd
Brittney Epperson <beppers87@hotmail.com>
2023-07-19 16:01:07 -0500

    Completed exercises and testing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0) {
+      return "OOPS";
+    } else if (n === 0) {
+      return 0;
+    }
 
+    let firstPrev = 1;
+    let seceondPrev = 0;
+
+    for (let i = 2; i <= n; i++) {
+      let current = firstPrev + seceondPrev;
+      seceondPrev = firstPrev;
+      firstPrev = current;
+    }
+  return firstPrev;
 };
 
 // Do not edit below this line
