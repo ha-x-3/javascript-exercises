bafe033d93671c38580611ff92a90e57674dc871
Brittney Epperson <beppers87@hotmail.com>
2023-07-19 11:08:17 -0500

    Finished exercise and testing.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,20 +6,36 @@ const subtract = function(a,b) {
 	return a - b;
 };
 
-const sum = function(a, b) {
-	return a + b;
+const sum = function(arr) {
+  let summedNums = 0;
+	for (let i = 0; i < arr.length; i++) {
+    summedNums += arr[i];
+  }
+  return summedNums;
 };
 
-const multiply = function(a, b) {
-	return a * b;
+const multiply = function(...arr) {
+	let multiplyNums = 1;
+	for (let i = 0; i < arr.length; i++) {
+    multiplyNums *= arr[i];
+  }
+  return multiplyNums;
 };
 
 const power = function(a, b) {
-	return a ^ b;
+	return Math.pow(a, b);
 };
 
-const factorial = function(a, b) {
-	return a ** b;
+const factorial = function(n) {
+	let factorialNum = 1;
+  if (n == 0 || n == 1) {
+    return factorialNum;
+  } else if (n > 1) {
+    for (let i = n; i >= 1; i--) {
+      factorialNum = factorialNum * i;
+    }
+    return factorialNum;
+  }
 };
 
 
