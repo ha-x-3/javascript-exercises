0ae6e8d41e7e3ec5b35205b58ee4b12d43186ca9
Brittney Epperson <beppers87@hotmail.com>
2023-07-19 12:31:30 -0500

    Completed exercise and testing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+  let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{\}~ ';
+  function removePunctuation(string) {
+    return string
+            .split('')
+            .filter(function(letter) {
+              return punctuation.indexOf(letter) === -1;
+            })
+            .join('');
+  }
+  str = removePunctuation(str).toLowerCase();
+  let reversedStr = str.split('').reverse().join('');
+  if (str == reversedStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
